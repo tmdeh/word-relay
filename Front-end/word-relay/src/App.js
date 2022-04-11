02f8f06d2f347a32506d300a8734d7e665ad95e6
tmdeh <yus174113@gmail.com>
2022-04-11 20:09:25 +0900

    프론트 라우팅

--- Front-end/word-relay/src/App.js
+++ Front-end/word-relay/src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
+import Nickname from './component/Nickname';
+import Home from './component/Home';
+import Room from './component/Room';
+import CreateRoom from './component/CreateRoom';
 
 function App() {
   return (
     <div className="App">
-      <Route path="/"></Route>
-      <Route path="/home"></Route>
-      <Route path="/room/:id"></Route>
-      <Route path="/home/create"></Route>
+      <Routes>
+        <Route path="/" element={<Nickname />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/room/:id" element={<Room />}></Route>
+        <Route path="/home/create" element={<CreateRoom />}></Route>
+      </Routes>
     </div>
   );
 }
--- Front-end/word-relay/src/index.js
+++ Front-end/word-relay/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowswerRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowswerRouter>
+  <BrowserRouter>
     <App />
-  </BrowswerRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
