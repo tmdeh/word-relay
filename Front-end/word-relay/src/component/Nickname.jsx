a0b245146de533fd7fbbc4c72b8bc8925ca14a4c
tmdeh <yus174113@gmail.com>
2022-04-12 16:04:18 +0900

    로더 수정

--- Front-end/word-relay/src/component/Nickname.jsx
+++ Front-end/word-relay/src/component/Nickname.jsx
@@ -1,47 +1,51 @@
 import React, { useState } from "react";
 import axios from 'axios'
-import Model from './Model'
 import Loading from './Loading'
 
 const Nickname = () => {
-
   const [nickname, setNickname] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const onChange = (e) => {
     setNickname(e.target.value)
-    if(e.key === 'Enter') {
+    if (e.key === 'Enter') {
       send()
     }
   }
 
-  const send = async() => {
+  const send = async () => {
+    if (nickname === "") {
+      alert("닉네임 칸이 비었습니다.")
+      return
+    }
+    
     setIsLoading(true)
     let data = {
-      nickname : nickname
+      nickname: nickname
     }
     await axios.post("http://localhost:8080/nickname", data)
-    .then((res) => {
-      console.log(res)
-      if(res.status === 201) {
-        localStorage.setItem('token', res.data.token)
-        window.location.href = "/home"
-      }
-    })
-    .catch((err) => {
-      alert("이미 사용중인 닉네임 입니다.")
-    })
+      .then((res) => {
+        console.log(res)
+        if (res.status === 201) {
+          localStorage.setItem('token', res.data.token)
+          window.location.href = "/home"
+        }
+      })
+      .catch((err) => {
+        alert("이미 사용중인 닉네임 입니다.")
+      })
     setIsLoading(false)
   }
 
-  return(
+  return (
     <div>
       <h1>끝말잇기</h1>
-      <input type="text" onKeyDown={onChange}/>
+      <input type="text" onKeyDown={onChange} />
       <button onClick={send}>시작</button>
       <Loading isLoading={isLoading} type="spin" color="99EA97"></Loading>
     </div>
   )
+
 }
 
 export default Nickname
