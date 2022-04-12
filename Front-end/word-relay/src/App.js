ba9c92d23ff8c7512313d46df688bd3c590fe48a
tmdeh <yus174113@gmail.com>
2022-04-12 20:56:55 +0900

    ui 개선

--- Front-end/word-relay/src/App.js
+++ Front-end/word-relay/src/App.js
@@ -4,17 +4,24 @@ import Nickname from './component/Nickname';
 import Home from './component/Home';
 import Room from './component/Room';
 import CreateRoom from './component/CreateRoom';
+import styled from "styled-components";
 
 function App() {
+
+  const AppDiv = styled.div`
+    width: 100vw;
+    height: 100vh;
+  `
+
   return (
-    <div className="App">
+    <AppDiv>
       <Routes>
         <Route path="/" element={<Nickname />}></Route>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/room/:id" element={<Room />}></Route>
         <Route path="/home/create" element={<CreateRoom />}></Route>
       </Routes>
-    </div>
+    </AppDiv>
   );
 }
 
--- Front-end/word-relay/src/component/Nickname.jsx
+++ Front-end/word-relay/src/component/Nickname.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from 'axios'
 import Loading from './Loading'
+import styled from "styled-components";
 
 const Nickname = () => {
   const [nickname, setNickname] = useState("");
@@ -32,20 +33,69 @@ const Nickname = () => {
         }
       })
       .catch((err) => {
+        console.log(err)
         alert("이미 사용중인 닉네임 입니다.")
       })
     setIsLoading(false)
   }
 
   return (
-    <div>
-      <h1>끝말잇기</h1>
-      <input type="text" onKeyDown={onChange} />
-      <button onClick={send}>시작</button>
-      <Loading isLoading={isLoading} type="spin" color="99EA97"></Loading>
-    </div>
+    <Background>
+      <Main>
+        <Title>끝말잇기</Title>
+        <NicknameInput placeholder="닉네임을 입력하세요." type="text" onKeyDown={onChange} />
+        <StartButton onClick={send}>시작</StartButton>
+        <Loading isLoading={isLoading} type="spin" color="99EA97"></Loading>
+      </Main>
+    </Background>
   )
 
 }
+
+const Title = styled.div`
+font-size: 96px;
+font-weight: bold;
+text-align: center;
+`
+
+const Background = styled.div`
+margin-top: 10rem;
+width:100%;
+height:100%;
+`
+
+const Main = styled.div`
+width: 40rem;
+height: 35rem;
+margin: auto;
+display: grid;
+justify-items: center;
+`
+
+const NicknameInput = styled.input`
+border-radius: 15px;
+width: 600px;
+height: 40px;
+outline: none;
+border-color: #9EDA9D;
+&:focus {
+  outline: none;
+}
+`
+
+const StartButton = styled.button`
+  width: 370px;
+  height: 75px;
+  border-radius: 25px;
+  border: none;
+  font-size: 30px;
+  background-color: #99EA97;
+  box-shadow: 2px 2px 2px 2px gray;
+  &:active {
+    margin-top: 5px;
+    margin-left: 5px;
+    box-shadow: none;
+  }
+`
 
 export default Nickname
