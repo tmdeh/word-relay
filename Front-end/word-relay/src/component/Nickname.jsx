ac22e7824c2a5149ab2fd295d67671211869b5da
tmdeh <yus174113@gmail.com>
2022-04-13 10:10:31 +0900

    네트워크 통신 에러 핸들링 추가

--- Front-end/word-relay/src/component/Nickname.jsx
+++ Front-end/word-relay/src/component/Nickname.jsx
@@ -6,6 +6,7 @@ import styled from "styled-components";
 const Nickname = () => {
   const [nickname, setNickname] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  
 
   const onChange = (e) => {
     setNickname(e.target.value)
@@ -33,8 +34,17 @@ const Nickname = () => {
         }
       })
       .catch((err) => {
-        console.log(err)
-        alert("이미 사용중인 닉네임 입니다.")
+        if (err.message === "Network Error") {
+          alert("네트워크 연결을 확인하세요")
+        } else {
+          if (err.response.data.status === 400 || err.response.data.message === "이미 존재하는 닉네임입니다.") {
+            alert("이미 사용중인 닉네임 입니다.")
+          } else if(err.response.data.status === 500) {
+            alert("서버 오류 발생")
+          } else {
+            alert("알수 없는 오류 발생")
+          }
+        }
       })
     setIsLoading(false)
   }
